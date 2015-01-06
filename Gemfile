d4427cf56e26e298e86045b50d8dd176a2771928
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-01-06 12:41:13 +0000

    implemented gov uk and moj elements for holding page

--- Gemfile
+++ Gemfile
@@ -1,12 +1,17 @@
 source 'https://rubygems.org'
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
 # Use postgresql as the database for Active Record
 gem 'pg'
+
+# Enable HAML (required for MOJ toolkit)
+gem 'haml-rails'
+
 # Use SCSS for stylesheets
+gem 'sprockets', '~> 2.12.3'
 gem 'sass-rails', '~> 4.0.3'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,46 @@ module FrBackoffice
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = :en
+
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => '',
+        'X-Content-Type-Options' => '',
+        'X-XSS-Protection' => '',
+        'Pragma' => '',
+        'Cache-Control' => '',
+        'Expires' => ''
+    } # let nginx set these headers
+
+    config.relative_url_root = ENV['RAILS_RELATIVE_URL_ROOT'] || ''
+
+    # disable default <div class="field_with_errors"> wrapping idiocy
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      html_tag
+    }
+
+    # app title appears in the header bar
+    config.app_title = 'Fee Remission'
+    config.proposition_title = 'Fee Remission - back office'
+    # phase governs text indicators and highlight colours
+    # presumed values: alpha, beta, live
+    config.phase = 'alpha'
+    # product type may also govern highlight colours
+    # known values: information, service
+    config.product_type = 'service'
+    # Feedback URL (URL for feedback link in phase banner)
+    # Use 'auto_add_path' for it to add a path link to the new_feedback route
+    config.feedback_url = config.relative_url_root #+ '/feedback/new'
+
+    config.autoload_paths += Dir["#{Rails.root}/app/lib/**/"]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.precompile += %w(
+      govuk_elements_extensions_ie6.css
+      govuk_elements_extensions_ie7.css
+      govuk_elements_extensions_ie8.css
+    )
   end
 end
