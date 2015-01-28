697ba83889dd65934296cc1e92c0830eb48017b0
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-01-28 13:10:19 +0000

    reset migration to original state

--- db/migrate/20150127103611_add_case_type_to_claims.rb
+++ db/migrate/20150127103611_add_case_type_to_claims.rb
@@ -2,8 +2,8 @@ class AddCaseTypeToClaims < ActiveRecord::Migration
   def change
     add_column :claims, :new_case, :boolean
     add_column :claims, :case_number, :string
-    # add_column :claims, :refund, :boolean
-    # add_column :claims, :refund_reference_number, :string
-    # add_column :claims, :fee_paid_date, :date
+    add_column :claims, :refund, :boolean
+    add_column :claims, :refund_reference_number, :string
+    add_column :claims, :fee_paid_date, :date
   end
 end
